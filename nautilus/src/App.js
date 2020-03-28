415f599b25a4c870a8854881ef069adffc2987a5
Student_M14 <student_m14@PS-DAL-M14.home>
2020-03-28 14:11:54 -0500

    5th commit

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -16,6 +16,8 @@ export default class app extends Component {
           </ul>
           <div class="header">
             <p1 class="main">THE NAUTILUS SPOTLIGHT</p1>
+            <h1 class="h1">WHY THE LAWS OF PHYSICS ARE INEVITABLE</h1>
+            <button>READ NOW</button>
           </div>
       </div>
     )
