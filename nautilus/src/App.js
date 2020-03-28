997f472cd39a01986e74684bc15f4c34bc3476e3
Student_M14 <student_m14@PS-DAL-M14.home>
2020-03-28 14:03:52 -0500

    4th commit

--- nautilus/src/App.js
+++ nautilus/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class app extends Component {
+
+
+  render() {
+    return (
+      <div>
+          <ul class="Navbar">
+            <li>Issues</li>
+            <li>Topics</li>
+            <li>Blogs</li>
+            <li>Newsletters</li>
+          </ul>
+          <div class="header">
+            <p1 class="main">THE NAUTILUS SPOTLIGHT</p1>
+          </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+
